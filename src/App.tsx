001f9ed268094dabcb7a11b21e28bdf2f37c8ae2
服部明諭 <thunaotoko@gmail.com>
2020-12-04 22:07:05 +0900

    add react-router^dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Provider } from 'react-redux';
+import Store from './store';
+import { Home } from "./components/Home"
+
+
+
+class App extends React.Component {
+
+  render(){
+    return (
+      <Provider store={Store}>
+        <Router>
+          <Route path="/" component={Home}></Route>
+        </Router>
+
+    </Provider>
+    );
+
+  }
+
 }
 
 export default App;
